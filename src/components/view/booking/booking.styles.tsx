31dcba71a41c19029c2f758633d4709cb0d45685
Miss-Sagar <misssagar02@gmail.com>
2024-02-16 23:16:07 +0530

    booking foam fixes

--- src/components/view/booking/booking.styles.tsx
+++ src/components/view/booking/booking.styles.tsx
@@ -1,1 +1,63 @@
 import styled from "styled-components";
+import Grid from '@mui/material/Grid';
+
+export const Heading = styled.div`
+    color: white;
+    padding: 5x;
+    font-size: 20px;
+    font-weight: 500;
+    text-align: center;
+`
+export const SmallHeading = styled.div`
+    color: white;
+    padding: 5x;
+    font-size: 15px;
+    font-weight: 300;
+    text-align: center;
+`
+export const BookingWayCont = styled.div`
+    // margin: 10% 0%;
+`;
+
+export const BookingWay = styled.div`
+`;
+
+export const BookingGridCont = styled(Grid)`
+    margin: 5% 0px 0px 0px !important;
+    padding: 0px !important;
+    display: flex !important;
+    flex-direction: raw !important;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    // border: 2px solid yellow;
+
+    @media screen and (max-width: 620px) {
+        flex-direction: column !important;
+    }
+`
+
+export const BookingTypeDiv = styled.div`
+    width: 100%;
+    padding: 20px;
+`
+
+export const ChooseSubCont = styled.div`
+    display: flex;
+    justify-content: space-around;
+    padding: 20px;
+
+    @media screen and (max-width: 420px) {
+        flex-direction: column ;
+    }
+`
+
+export const ChooseCont = styled.div`
+    display: flex;
+    flex-direction: column;
+    color: white;
+    padding: 20px 40px;
+    border: 1px solid white;
+    border-radius: 10px;
+    cursor: pointer;
+`
--- src/components/view/booking/booking.tsx
+++ src/components/view/booking/booking.tsx
@@ -11,14 +11,8 @@ import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import Dropdown from '../../common/dropdown/dropdown';
 import DropDown from '../../common/dropdown/dropdown';
+import { Heading, SmallHeading, BookingWayCont, BookingWay, BookingGridCont, BookingTypeDiv, ChooseSubCont, ChooseCont } from './booking.styles';
 
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: '#23a584',
-    // padding: '10px',
-    textAlign: 'center',
-    border: '1px solid red',
-    borderRadius: '100%'
-}));
 
 const Booking = () => {
     const [count, setCount] = useState(0);
@@ -33,93 +27,85 @@ const Booking = () => {
         }
     };
     return (
-        <div>
-            <div style={{ margin: '0px 20%' }}>
-                <div style={{ border: '2px solid grey', position: 'relative' }}>
-                    <div style={{ color: 'white', padding: '5px', fontSize: '20px', fontWeight: '500', textAlign: 'center' }}>3 Steps Easy Booking</div>
-                    <div style={{ color: 'white', padding: '5px', fontSize: '15px', fontWeight: '300', textAlign: 'center' }}>Used a bunch of party options with just a few simple steps</div>
-                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin: '40px 0px 20px 0px' }}>
-                        <Grid container spacing={4} >
-                            <Grid item xs={4} style={{ textAlign: 'center' }}>
-                                {/* <div style={{ alignItems: 'center', backgroundColor: 'green', textAlign: 'center', borderRadius: '100%', paddingTop: '', height: '100px', width: '100px' }}> */}
-                                <MessageIcon style={{ marginTop: '35px', color: 'white' }} />
-                                {/* </div> */}
-                                <div style={{ padding: '20px', color: 'white' }}>Title1</div>
-                            </Grid>
-                            <Grid item xs={4} style={{ textAlign: 'center' }}>
-                                {/* <div style={{ backgroundColor: 'green', textAlign: 'center', borderRadius: '100%', padding: '', height: '100px', width: '100px' }}> */}
-                                <GavelIcon style={{ marginTop: '35px', color: 'white' }} />
-                                {/* </div> */}
-                                <div style={{ padding: '20px', color: 'white' }}>Title1</div>
-                            </Grid>
-                            <Grid item xs={4} style={{ textAlign: 'center' }}>
-                                {/* <div style={{ backgroundColor: 'green', textAlign: 'center', borderRadius: '100%', padding: '', height: '100px', width: '100px' }}> */}
-                                <CameraAltRoundedIcon style={{ marginTop: '35px', color: 'white' }} />
-                                {/* </div> */}
-                                <div style={{ padding: '20px', color: 'white' }}>Title1</div>
-                            </Grid>
-                        </Grid>
-                    </div>
-                    <div style={{ position: 'absolute',bottom: '-25px', left: '30%', right: '40%', textAlign: 'center', padding: '40px 40px -40px 40px' }}>
-                        <div style={{ width: '100%', textAlign: 'center', margin: 'auto', color: 'white', backgroundColor: '#d6144a', padding: '10px 40px 10px 40px', borderRadius: '30px' }}>Share Requirement</div>
-                    </div>
-                </div>
-                <div>
-                    <div style={{ color: 'white', padding: '20px' }}>Choose your club</div>
-                    <div style={{ display: 'flex', justifyContent: 'space-between', padding: '0px 20px' }}>
-                        <div style={{ display: 'flex', flexDirection: 'column', color: 'white', padding: '20px 40px', border: '1px solid white', borderRadius: '10px' }}>
-                            <Checkbox />
-                            Club 1
-                        </div>
-                        <div style={{ display: 'flex', flexDirection: 'column', color: 'white', padding: '20px 40px', border: '1px solid white', borderRadius: '10px' }}>
-                            <Checkbox />
-                            Club 2
-                        </div>
-                        <div style={{ display: 'flex', flexDirection: 'column', color: 'white', padding: '20px 40px', border: '1px solid white', borderRadius: '10px' }}>
-                            <Checkbox />
-                            Club 3
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <div>
-                        <div style={{ color: 'white', padding: '20px' }}>Come to the party</div>
-                        <div style={{ display: 'flex', justifyContent: 'space-between', padding: '0px 20px' }}>
-                            <div style={{ display: 'flex', flexDirection: 'column', color: 'white', padding: '30px 40px', border: '1px solid white', borderRadius: '10px' }}>
-                                <div>Today</div>
-                                <div>11:00 AM</div>
+            <div style={{ border: '2px solid orange' }}>
+            <BookingWayCont style={{ border: '2px solid grey', position: 'relative', padding: '5% 10% ' }}>
+                <Heading>3 Steps Easy Booking</Heading>
+                <SmallHeading>Used a bunch of party options with just a few simple steps</SmallHeading>
+                <BookingWay>
+                    <BookingGridCont>
+                        <BookingTypeDiv>
+                            <div style={{ borderRadius: '100%', display: 'flex', textAlign: 'center', justifyContent: 'center', margin: '0px 30%', padding: '15% 30px', alignItems: 'center', color: 'white', backgroundColor: 'green' }}>
+                                <MessageIcon style={{ color: 'white' }} />
                             </div>
-                            <div style={{ display: 'flex', flexDirection: 'column', color: 'white', padding: '30px 40px', border: '1px solid white', borderRadius: '10px' }}>
-                                Tomorrow
-                                <div>11:00 AM</div>
+                            <div style={{ padding: '10px 0px', color: 'white', textAlign: 'center', width: '100%' }}>Title1</div>
+                        </BookingTypeDiv>
+                        <BookingTypeDiv>
+                            <div style={{ borderRadius: '100%', display: 'flex', textAlign: 'center', justifyContent: 'center', margin: '0px 30%', padding: '15% 0px', alignItems: 'center', color: 'white', backgroundColor: 'green' }}>
+                                <GavelIcon style={{ color: 'white' }} />
                             </div>
-                            <div style={{ display: 'flex', flexDirection: 'column', color: 'white', padding: '30px 40px', border: '1px solid white', borderRadius: '10px' }}>
-                                Yesterday
-                                <div>11:00 AM</div>
+                            <div style={{ padding: '10px 0px', color: 'white', textAlign: 'center', width: '100%' }}>Title1</div>
+                        </BookingTypeDiv>
+                        <BookingTypeDiv>
+                            <div style={{ borderRadius: '100%', display: 'flex', textAlign: 'center', justifyContent: 'center', margin: '0px 30%', padding: '15% 0px', alignItems: 'center', color: 'white', backgroundColor: 'green' }}>
+                                <CameraAltRoundedIcon style={{ color: 'white' }} />
                             </div>
-                        </div>
-                    </div>
-                </div>
+                            <div style={{ padding: '10px 0px', color: 'white', textAlign: 'center', width: '100%' }}>Title1</div>
+                        </BookingTypeDiv>
+                    </BookingGridCont>
+                </BookingWay>
+            </BookingWayCont>
 
-                <div>
-                    <div>
-                        <div style={{ color: 'white', padding: '20px' }}>Party Time</div>
-                        <div style={{ display: 'flex', justifyContent: 'space-between', padding: '0px 20px' }}>
-                            <div style={{ display: 'flex', flexDirection: 'column', color: 'white', padding: '40px', border: '1px solid white', borderRadius: '10px' }}>
-                                <div>11:00 AM</div>
-                            </div>
-                            <div style={{ display: 'flex', flexDirection: 'column', color: 'white', padding: ' 40px', border: '1px solid white', borderRadius: '10px' }}>
-                                <div>11:00 AM</div>
-                            </div>
-                            <div style={{ display: 'flex', flexDirection: 'column', color: 'white', padding: '40px', border: '1px solid white', borderRadius: '10px' }}>
-                                <div>11:00 AM</div>
-                            </div>
+            <div>
+                <Heading>Choose your club</Heading>
+                <ChooseSubCont>
+                    <ChooseCont>
+                        <Checkbox style={{ color: 'white' }} />
+                        Club 1
+                    </ChooseCont>
+                    <ChooseCont>
+                        <Checkbox style={{ color: 'white' }} />
+                        Club 2
+                    </ChooseCont>
+                    <ChooseCont>
+                        <Checkbox style={{ color: 'white' }} />
+                        Club 3
+                    </ChooseCont>
+                </ChooseSubCont>
+            </div>
 
-                        </div>
+            <div>
+                <Heading>Come to the party</Heading>
+                <ChooseSubCont>
+                    <ChooseCont>
+                        <div>Today</div>
+                        <div>11:00 AM</div>
+                    </ChooseCont>
+                    <ChooseCont>
+                        Tomorrow
+                        <div>11:00 AM</div>
+                    </ChooseCont>
+                    <ChooseCont>
+                        Yesterday
+                        <div>11:00 AM</div>
+                    </ChooseCont>
+                </ChooseSubCont>
+            </div>
 
-                    </div>
-                </div>
+            <div>
+                <Heading>Party Time</Heading>
+                <ChooseSubCont>
+                    <ChooseCont>
+                        <div>11:00 AM</div>
+                    </ChooseCont>
+                    <ChooseCont>
+                        <div>11:00 AM</div>
+                    </ChooseCont>
+                    <ChooseCont>
+                        <div>11:00 AM</div>
+                    </ChooseCont>
 
+                </ChooseSubCont>
+            </div>
 
                 <div style={{ padding: '20px 20px', borderBottom: '3px solid grey', borderRadius: '5px' }}>
                     <div style={{ padding: '10px 0px', display: 'flex', justifyContent: 'space-between', alignItems: 'center', color: 'white' }}>
@@ -158,6 +144,7 @@ const Booking = () => {
                         <DropDown />
                     </div>
                 </div>
+                
                 <div style={{ display: 'flex', justifyContent: 'space-between', color: 'white', padding: '20px' }}>
                     <div>
                         <div style={{ padding: '20px 0px' }}>Please Choose</div>
@@ -169,7 +156,9 @@ const Booking = () => {
                     </div>
                 </div>
             </div>
-        </div>
+           
+        
+       
     )
 }
 
